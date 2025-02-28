051ffe341de4a13c9a60c74eff8df5b6f6039eb6
TheReelJC <jasper@thejcloud.com>
2025-02-28 11:30:28 +0000

    cool thing

--- coolfilething.py
+++ coolfilething.py
@@ -1,4 +1,4 @@
 print("Hi I am Jasper Cotton. (Cool coder guy.)")
-print("I am creating 2 vr games at once. (MineTag and Sliver Runners.")
+print("I am creating 2 vr games at once. (MineTag and Sliver Runners.)")
 print("I have 2 brothers who test my vr game.")
 print("My vr does not work because the charging port broke.")
